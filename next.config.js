af9cc517aab9669a3c201ae232fca5d9aa49d141
RyanMKrol <ryankrol.m@gmail.com>
2025-08-24 21:19:09 +0100

    Fix next.config.js warnings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  experimental: {
-    // Temporary workaround for Next.js src directory support
-    srcDir: true
-  }
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx']
 }
 
 module.exports = nextConfig
